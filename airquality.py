fc7e7cbb9e317ef41422696615b8005cf495510b
ssiegl <func01@siegl.mobi>
2020-03-21 14:57:05 +0100

    add kinesis first steps

--- airquality.py
+++ airquality.py
@@ -3,7 +3,7 @@ import boto3
 import os
 import json
 
- 
+
 
 
 url = 'https://api.waqi.info/feed/berlin/?token='+os.environ['AIR_QUALITY_API_TOKEN']
@@ -12,3 +12,11 @@ response = requests.get(url)
 response_json=response.json()
 
 print(json.dumps(response_json, indent=2, sort_keys=True))
+
+
+client = boto3.client('kinesis')
+#Records = [{"Data" : json.dumps(response_json, indent=2, sort_keys=True)}
+
+response = client.put_record(StreamName='air_quality_test', Data=json.dumps(response_json, indent=2, sort_keys=True),PartitionKey='part_key_1')
+
+print (response)
